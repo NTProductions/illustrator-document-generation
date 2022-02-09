fc34c707370cb4d705c1b2b42dae751c5240cb3f
Nate Lovell <Natelovell99@gmail.com>
2022-02-08 19:28:53 -0700

    Create Illustrator Document Generation.jsx

---
+++ Illustrator Document Generation.jsx
@@ -0,0 +1,29 @@
+// Illustrator Document Creation
+
+// documentColorSpace-
+	// DocumentColorSpace.RGB - 1
+	// DocumentColorSpace.CMYK - 2
+// artboardLayout-
+	// DocumentArtboardLayout.GridByRow - 1
+	// DocumentArtboardLayout.GridByCol - 2
+	// DocumentArtboardLayout.Row - 3
+	// DocumentArtboardLayout.Column - 4
+	// DocumentArtboardLayout.RLGridByRow - 5
+	// DocumentArtboardLayout.RLGridByCol - 6
+	// DocumentArtboardLayout.RLRow - 7
+// artboardSpacing-
+	// use a number
+// artboardRowsOrCols-
+	// use a number
+// startupPreset-
+	// string
+// presetSettings-
+	// 0
+// showOptionsDialog-
+	// true || false
+
+app.documents.add(DocumentColorSpace.RGB, 1920, 1080, 5, DocumentArtboardLayout.RLGridByRow, 20, 4);
+
+app.documents.addDocument("Web");
+
+app.documents.addDocumentWithDialogOption("Web", true);
